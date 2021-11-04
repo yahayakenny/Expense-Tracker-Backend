2e5803245b999b46c67038a580fd72caf5f058e0
yahayakenny <y.hkehinde@yahoo.com>
2021-11-04 15:51:06 +0000

    added db count

--- backend/settings.py
+++ backend/settings.py
@@ -19,14 +19,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECRET_KEY = 'django-insecure-t6_*1w(=78-45p$1lusa0ig0ox%00dafg+z++1dz6qr0r(d#!c'
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('api/', include('income.urls')),
     path('api/', include('users.urls')),  
     path('api/', include('core.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
