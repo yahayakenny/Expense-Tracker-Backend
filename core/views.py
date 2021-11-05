c388f2b89ed8816ab20af5073ec022c02be6835f
yahayakenny <y.hkehinde@yahoo.com>
2021-11-05 11:54:34 +0000

    bug: fixed current user not displaying

--- core/views.py
+++ core/views.py
@@ -58,7 +58,7 @@ class QueryDayGraph(APIView):
         delta = today - one_week_ago  
         for i in range(delta.days + 1):
             day = one_week_ago + timedelta(days=i)
-            queryset = Expense.objects.filter(date__startswith = day)
+            queryset = Expense.objects.filter(user=request.user).filter(date__startswith = day)
             day_cost = sum([expense.amount for expense in queryset])
             data.append({"day": day,"amount": day_cost})
         return Response({"filtered": data})
@@ -86,22 +86,22 @@ class QueryMonthGraph(APIView):
 class QueryMostRecentView(APIView):
     permission_class = [IsAuthenticated]
     def get(self, request):
-        filtered = Expense.objects.all().filter(user=request.user).order_by('-id')[:5]
+        filtered = Expense.objects.filter(user=request.user).order_by('-id')[:5]
         serializer = ExpenseSerializer(filtered, many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class QueryNetView(APIView):
     permission_class = [IsAuthenticated]
     def get(self, request):
-        total_expenses = Expense.objects.filter(date__month = str(current_month))
-        expense_count = Expense.objects.filter(date__month = str(current_month)).all().count()
+        total_expenses = Expense.objects.filter(user=request.user).filter(date__month = str(current_month))
+        expense_count = Expense.objects.filter(user=request.user).filter(date__month = str(current_month)).all().count()
        
         expense_sum = round((sum(expense.amount  for expense in total_expenses)), 2)
-        total_income = Income.objects.filter(date__month = str(current_month))
-        income_count = Income.objects.filter(date__month = str(current_month)).all().count()
+        total_income = Income.objects.filter(user=request.user).filter(date__month = str(current_month))
+        income_count = Income.objects.filter(user=request.user).filter(date__month = str(current_month)).all().count()
       
         income_sum = round((sum(income.amount  for income in total_income)), 2)
-        category_count = Category.objects.filter(date__month = str(current_month)).all().count()
+        category_count = Category.objects.filter(user=request.user).filter(date__month = str(current_month)).all().count()
 
         net_value = income_sum-expense_sum
         return Response({"expense": expense_sum , "income": income_sum, "net":net_value, "incomeCount": income_count,"expenseCount": expense_count, "categoryCount":category_count }, status=status.HTTP_200_OK, )
@@ -110,7 +110,7 @@ class QueryNetView(APIView):
 class QueryCategoryView(APIView):
     permission_class = [IsAuthenticated]
     def get(self, request):
-        categories = Category.objects.filter(date__month = str(current_month))
+        categories = Category.objects.filter(user=request.user).filter(date__month = str(current_month))
         data = []
         for i in categories:
             data.append({"category": i.name,"amount": i.total_expense_cost})
