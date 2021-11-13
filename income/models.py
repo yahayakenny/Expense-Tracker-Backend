13927ec3ba396b47bca5ca080e885b930e8ce3a2
yahayakenny <y.hkehinde@yahoo.com>
2021-11-13 18:56:45 +0000

    fixed income bug

--- income/models.py
+++ income/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Income(models.Model):
     name = models.CharField(max_length=30,blank=True, null=True)
-    amount = models.PositiveIntegerField(default = 0)
+    amount = models.FloatField(default = 0, blank=True)
     user = models.ForeignKey(User,related_name = 'incomes', on_delete=models.SET_NULL, null = True )
     description = models.TextField(null=True, blank=True)
     date = models.DateField(auto_now=True)
