db352f12f563963ad2044d376eb78d1d42f2c89b
yahayakenny <y.hkehinde@yahoo.com>
2022-01-22 18:34:55 +0000

    bug fixes

--- expense/tests/test_urls.py
+++ expense/tests/test_urls.py
@@ -1,9 +1,9 @@
-from django.test import SimpleTestCase
+from rest_framework.test import APITestCase
 from django.urls import reverse, resolve
 from expense.views import ExpenseListView, ExpenseDetailView
 
 
-class TestExpenseUrls(SimpleTestCase):
+class TestExpenseUrls(APITestCase):
     def test_expense_list_url_is_resolved(self):
         url = reverse("expense:expense_list")
         self.assertEquals(resolve(url).func.view_class, ExpenseListView)
--- income/tests/test_urls.py
+++ income/tests/test_urls.py
@@ -1,9 +1,9 @@
-from django.test import SimpleTestCase
+from rest_framework.test import APITestCase
 from django.urls import reverse, resolve
 from income.views import IncomeListView, IncomeDetailView
 
 
-class TestIncomeUrls(SimpleTestCase):
+class TestIncomeUrls(APITestCase):
     def test_income_list_url_is_resolved(self):
         url = reverse("income:income_list")
         self.assertEquals(resolve(url).func.view_class, IncomeListView)
